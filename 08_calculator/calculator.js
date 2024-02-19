ae85c6325bc635f9f94e2fcf1937269d606435dc
Renan Pacini <renanpacini@gmail.com>
2024-02-19 16:01:31 -0300

    Solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(val1, val2) {
+	return val1+val2;
 };
 
-const subtract = function() {
-	
+const subtract = function(val1, val2) {
+	return val1-val2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(val,power) {
+	return Math.pow(val,power);
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+  if (value === 0) return 1;
+
+	let factorialResult = 1;
+
+  for (i = 1; i <= value; i++) {
+    factorialResult = factorialResult * i;
+  }
+  return factorialResult;
 };
 
 // Do not edit below this line
