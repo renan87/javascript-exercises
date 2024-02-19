9feb3272fd99b9a3eb2d091b691c295ac40e42c2
Renan Pacini <renanpacini@gmail.com>
2024-02-19 16:17:40 -0300

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (stringInput) {
 
+    const normalizedString = stringInput.toLowerCase().replace(/[\W_]/g, "");
+    const array = normalizedString.split("");
+    const reverseText = array.reverse().join("");
+
+
+    if (normalizedString === reverseText) {
+        return true;
+    }
+    else return false;
 };
 
 // Do not edit below this line
