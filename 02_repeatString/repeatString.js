2bf732b1dd241d8da2e250020c3ae00b72c40ab6
Renan Pacini <renanpacini@gmail.com>
2024-02-19 13:22:31 -0300

    Solved exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, number) {
+    let textArray = [];
+    for (i = 0; i < number; i++) {
+        textArray.push(text);
+    }
+    let string = textArray.join("");
+    return string
 };
 
 // Do not edit below this line
