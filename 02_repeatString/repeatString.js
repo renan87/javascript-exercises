88bce5c72ab4f16e916f4287aaad1a9a94233a0b
Renan Pacini <renanpacini@gmail.com>
2024-02-19 13:46:55 -0300

    Fixed exercise 02 to pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,7 @@
 const repeatString = function(text, number) {
+    if (number < 0) {
+        return "ERROR"
+    }
     let textArray = [];
     for (i = 0; i < number; i++) {
         textArray.push(text);
