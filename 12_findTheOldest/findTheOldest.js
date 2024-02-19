c261b44b08dfeb633a3c4073e9eab492417ed2f0
Renan Pacini <renanpacini@gmail.com>
2024-02-19 19:32:42 -0300

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldestPerson = array.reduce((oldest, currentPerson) => {
+        const oldestAge = (oldest.yearOfDeath || new Date().getFullYear()) - oldest.yearOfBirth;
+        const currentAge = (currentPerson.yearOfDeath || new Date().getFullYear()) - currentPerson.yearOfBirth;
+      
+        return oldestAge > currentAge ? oldest : currentPerson;
+      });
+    return oldestPerson;  
 };
 
 // Do not edit below this line
