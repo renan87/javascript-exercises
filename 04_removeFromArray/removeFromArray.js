eae18c2b8afa96e051ae2913f88a258a5c10dfed
Renan Pacini <renanpacini@gmail.com>
2024-02-19 14:29:31 -0300

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(input, ...removeFrom) {
+    let initialArray = input;
+    let removeArray = [...removeFrom]; 
+    let removeArraySize = removeArray.length;
 
+    let newArray = initialArray.filter(element => !removeArray.includes(element));
+    return newArray;
 };
 
 // Do not edit below this line
