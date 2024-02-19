acbe65573ef8e5e19ae8268c1f8a63d8954b4d44
Renan Pacini <renanpacini@gmail.com>
2024-02-19 18:41:58 -0300

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(numberPosition) {
 
+    numberPosition = +numberPosition;
+    
+    let previousNumber = 0, fibonacciNumber = 1, temp;
+    
+    if (numberPosition < 0) return "OOPS";
+    if (numberPosition === 0) return 0; 
+    if (numberPosition === 1) return fibonacciNumber;
+    
+    for (let i = 2; i <= numberPosition; i++) {
+        temp = previousNumber + fibonacciNumber;
+        previousNumber = fibonacciNumber;
+        fibonacciNumber = temp;
+    }
+    return fibonacciNumber;
 };
 
 // Do not edit below this line
