3b39d827e039e96981bc0efd3b769fc2dfe27d2b
Renan Pacini <renanpacini@gmail.com>
2024-02-19 13:39:36 -0300

    Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(textInput) {
 
+    const array = textInput.split("");
+    const reverseArray = array.reverse();
+    const reverseText = reverseArray.join("");
+    return reverseText;
 };
 
 // Do not edit below this line
