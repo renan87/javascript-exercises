5f4c510fa9014eac65b05dd66e1baf1192c860a4
Renan Pacini <renanpacini@gmail.com>
2024-02-19 14:45:29 -0300

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0 || typeof min !== 'number' || typeof max !== 'number') {
+        return "ERROR";
+    }
 
+    if(min > max) {
+        let temp = min;
+        min = max;
+        max = temp;
+    }
+
+    let sumOfAll = 0;
+    for (let i = min; i <= max; i++) {
+        sumOfAll = sumOfAll + i;
+    }
+    return sumOfAll; 
+ 
 };
 
 // Do not edit below this line
