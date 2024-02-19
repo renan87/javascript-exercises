0f9fc6e40435c69bc06cdbe1ab78dc7e9d2fde16
Renan Pacini <renanpacini@gmail.com>
2024-02-19 15:26:43 -0300

    Solve exercise 07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let newTemp = Math.round(((fTemp-32)*(5/9))*10)/10;
+  return newTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let newTemp = Math.round(((cTemp*(9/5))+32)*10)/10;
+  return newTemp;
 };
 
 // Do not edit below this line
